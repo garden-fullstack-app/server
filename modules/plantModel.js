fbdd063a380258f347cb669b6d615d1d569bdc79
Joshua Huston <joshua.david.huston@gmail.com>
2021-11-11 00:44:30 -0500

    Test API and starter object created

---
+++ modules/plantModel.js
@@ -0,0 +1,21 @@
+// const mongoose = require('mongoose');
+
+// const { Schema } = mongoose;
+
+// const plantSchema = new.mongoose.Schema({
+//   plantName: { type: String },
+//   plantFamily: { type: String },
+//   determinate: { type: Boolean },
+//   directSowDate: { type: Date },
+//   daysToMaturity: { type: Number },
+//   harvestCountdown: { type: Number },
+//   lightRequirements: { type: String },
+//   fertilizing: { type: Object },
+//   harvest: { type: Object },
+//   companionPlants: { type: Array },
+//   enemyPlants: { type: Array }
+// })
+
+// const PlantModel = mongoose.model('gardenPlant', plantSchema);
+
+// module.exports = PlantModel;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const Data = require('./modules/data.js');
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+app.get('/crops', Data.accessCropAPI);
 
 app.get("/test", (req, res) => res.send("SERVER IS RUNNING"));
 
